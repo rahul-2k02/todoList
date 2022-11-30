f7548eb5cd0add5a019ac883ddb26822462b5163
rahul-2k02 <rahulraj.puthiyaveedu@science.christuniversity.in>
2022-11-30 20:04:23 +0530

    day4

--- todoApp/models.py
+++ todoApp/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class TodoListItem(models.Model):
     content = models.TextField() 
     date = models.DateField(auto_now_add=True)
+    user = models.CharField(max_length=100)
+    
 
 class TodoDone(models.Model):
     done = models.Exists
